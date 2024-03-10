f1eef889ae9146d532b9c42929113fb6b940dd37
Bob Zhang <141781876+BobZhang26@users.noreply.github.com>
2024-03-10 15:18:16 -0400

    Update test_main.rs

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,1 +1,9 @@
 // update this file with your own tests
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // The first argument is the path that was used to call the program.
+    println!("My path is {}.", args[0]);
+}
